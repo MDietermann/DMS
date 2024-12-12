b84a5ac073dd73df5b83d418683dd4ea8ff310f4
Marvin <marvin.dietermann@wagner-ecommerce.group>
2024-12-12 16:09:58 +0100

    removed unused imports

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,11 @@
 <script setup lang="ts">
 import DatabaseDisplay from "../components/database/DatabaseDisplay.vue";
-import { acceptedFileTypesExport, acceptedFileTypesImport } from "../mock/filetypes";
-import { resolveUserDataNames } from "../scripts/dbResolver";
-import Section from "../components/section/Section.vue";
 import { useEmployeeStore } from "../stores/employee";
 import Login from "../components/login/Login.vue";
-import { dbMock } from "../mock/db-mock";
 import { ref, watch } from "vue";
 
 const employeeStore = useEmployeeStore();
-const isLoggedIn = ref(false);
+const isLoggedIn = ref(employeeStore.isLoggedIn);
 
 watch(
     () => employeeStore.isLoggedIn,
@@ -17,10 +13,6 @@ watch(
         isLoggedIn.value = employeeStore.isLoggedIn;
     }
 );
-
-const validDataTypes = (typesArray: string[]) => {
-    return typesArray.join(", ");
-};
 </script>
 
 <template>
